de11d5ffcce327510569fe2e83bc53938e3d0753
pedrosrs <pedrosrs.dev@gmail.com>
2024-12-19 23:10:54 -0300

    Refactor data parser

--- data_parser/data_parser.py
+++ data_parser/data_parser.py
@@ -1,6 +1,5 @@
 import pika
 import sys
-import os
 import json
 import pandas as pd
 from sqlmodel import Field, SQLModel, create_engine, Session
@@ -14,11 +13,13 @@ import re
 POSTGRES_USERNAME = 'user'
 POSTGRES_PASSWORD = 'passwd'
 POSTGRES_DB_NAME = 'property-db'
+DB_URL = f'postgresql://{POSTGRES_USERNAME}:{POSTGRES_PASSWORD}@localhost:5432/{POSTGRES_DB_NAME}'
 
 RENT_TABLE_NAME = "property_rent"
 SALE_TABLE_NAME = "property_sale"
 
 class Property(SQLModel, table=False): 
+    source: str = None
     listing_id: str = None
     title: str
     price: Optional[float] = None
@@ -44,59 +45,40 @@ class Property(SQLModel, table=False):
         if value is None or value == '':
             return None
         value_str = str(value).strip()
-        if '5 ou mais' in value_str:
-            return 5
-        try:
-            return int(value_str)
-        except (ValueError, TypeError):
-            return None 
+        match = re.search(r'(\d+)', value_str)
+        if match:
+            return int(match.group(1))
+        return None
 
     @field_validator('listing_id', mode='before')
     def convert_listing_id_to_str(cls, value):
-        if isinstance(value, (int, float)):
-            return str(int(value))
-        return value
+        return str(int(value)) if isinstance(value, (int, float)) else value
 
     @field_validator('condominium_details', 'property_details', 'type', mode='before')
     def process_details(cls, value: Union[str, List[str], Set[str]]):
         if isinstance(value, str):
             return set(value.split(", ")) if ", " in value else {value}
-        elif isinstance(value, (list, set)):
-            return set(value)
-        return None
+        return set(value) if isinstance(value, (list, set)) else None
 
     @field_validator('listing_date', mode='before')
     def convert_epoch_to_datetime(cls, value):
-        if isinstance(value, int):
-            return datetime.fromtimestamp(value)
-        return value
+        return datetime.fromtimestamp(value) if isinstance(value, int) else value
 
     @field_validator('price', 'condominium_price', 'iptu', 'area', mode='before')
-    def convert_string_to_float(cls, value):
-        if value and isinstance(value, str) and value.lower is not 'nan':
-            value = re.sub(r'[^\d=]', '', value)
-            return float(value)
-        if isinstance(value, (int, float)):
-            return float(value)
-        return None
+    def convert_currency_to_float(cls, value):
+        if value and isinstance(value, str):
+            value = re.sub(r'[^0-9,\.]', '', value) 
+            value = float(value.replace('.', '').replace(',', '.')) 
+
+        return value if isinstance(value, (int, float)) else None
 
     @classmethod
     def create_dynamic_table(cls, table_name: str):
         metadata = MetaData()
         return Table(table_name, metadata, *cls.__table__.columns)
 
-def get_table_name(url: str) -> str:
-    if "venda" in url:
-        return SALE_TABLE_NAME
-    elif "aluguel" in url:
-        return RENT_TABLE_NAME
-    else:
-        raise ValueError(f"Unknown URL: {url}")
-
-def insert_data_to_db(table_name: str, model_dicts: List[Dict], model_class: SQLModel):
-    db_url = f'postgresql://{POSTGRES_USERNAME}:{POSTGRES_PASSWORD}@localhost/{POSTGRES_DB_NAME}'
-    engine = create_engine(db_url)
-    
+def insert_data_to_db(table_name: str, model_dicts: List[Dict]):
+    engine = create_engine(DB_URL)
     metadata = MetaData()
     dynamic_table = Table(table_name, metadata, autoload_with=engine)
 
@@ -106,135 +88,49 @@ def insert_data_to_db(table_name: str, model_dicts: List[Dict], model_class: SQL
             session.exec(stmt)
         session.commit()
 
-def parse_region(url):
-    if "?" in url:
-        pattern = re.compile(r'/([^?/]+)\?')
-        match = pattern.search(url)
-    else:
-        pattern = re.compile(r'/([^/]+)$')
-        match = pattern.search(url)
-
-    if match:
-        result = match.group(1).replace('-', ' ')
-        return result
-    else:
-        return None
-
-def rename_columns(data):
-    new_column_names = {
-        'title': 'title',
-        'price': 'price',
-        'listId': 'listing_id',
-        'url': 'property_url',
-        'date': 'listing_date',
-        'locationDetails.municipality': 'municipality',
-        'locationDetails.neighbourhood': 'neighborhood',
-        'locationDetails.uf': 'state',
-        'área construída': 'area',
-        'vagas na garagem': 'parking',
-        'quartos': 'bedrooms',
-        'banheiros': 'bathrooms',
-        'condomínio': 'condominium_price',
-        'categoria': 'category',
-        'tipo': 'type',
-        'detalhes do condomínio': 'condominium_details',
-        'detalhes do imóvel': 'property_details'
-    }
-    data.rename(columns=new_column_names, inplace=True)
-    return data
-
-def expand_properties(row):
-    property_dict = {}
-    properties = row.get('properties', [])
-
-    if isinstance(properties, list) and properties:
-        for item in properties:
-            label = item['label'].lower()
-            value = item['value']
-            property_dict[label] = value
-
-    return pd.Series(property_dict)
-
-def extract_fields(unfiltered_json):
-    data = pd.json_normalize(unfiltered_json)
-    property_stats = data.apply(expand_properties, axis=1)
-    data = pd.concat([data, property_stats], axis=1)
-
-    additional_columns = [
-        'title', 'price', 'listId', 'url', 'date',
-        'locationDetails.municipality',
-        'locationDetails.neighbourhood',
-        'locationDetails.uf'
-    ]
-
-    unparsed_data = data[additional_columns + property_stats.columns.tolist()]
-    return unparsed_data
-
-def prepare_dataset(raw_data):
-    unparsed_data = extract_fields(raw_data)
-    unparsed_data = rename_columns(unparsed_data)
-    print(unparsed_data.head())
-
-    model_dicts = df_to_sqlmodel_dicts(unparsed_data, Property)
-
-    output_df = pd.DataFrame(model_dicts)
-    output_df.to_csv('output.csv', index=False)
-
-def df_to_sqlmodel_dicts(df: pd.DataFrame, model_class: SQLModel) -> List[Dict]:
+def df_to_sqlmodel_dicts(data: List[Dict], model_class: SQLModel) -> List[Dict]:
     sqlmodel_dicts = []
-    for index, row in df.iterrows():
+    for record in data:
         try:
-            sqlmodel_instance = model_class(**row.to_dict())
-            sqlmodel_dict = sqlmodel_instance.model_dump()
-            sqlmodel_dicts.append(sqlmodel_dict)
+            df = pd.DataFrame(record, index=[0])
+            rows_as_dicts = df.to_dict(orient="records")
+            instances = [model_class(**row) for row in rows_as_dicts]
+            sqlmodel_dicts.extend(instance.model_dump() for instance in instances)
         except ValidationError as e:
-            print(f"Skipping row {index} due to validation error: {e}")
-
+            print(f"Skipping records due to validation error: {e}")
+        except Exception as e:
+            print(f"Error processing data: {e}")
     return sqlmodel_dicts
 
-def callback(ch, method, properties, body):
-    body_str = body.decode('utf-8')
-    print(body_str)
-
+def handle_message(body: bytes):
     try:
-        message = json.loads(body_str)
-        raw_data = json.loads(message['content'])
-        url = message['url']
+        message = json.loads(body.decode('utf-8'))
+        property_data = message['content']
+        table_name = message['ad_type']
 
-        print(raw_data)
+        model_dicts = df_to_sqlmodel_dicts(property_data, Property)
+        insert_data_to_db(table_name, model_dicts)
 
-        unparsed_data = extract_fields(raw_data['props']['pageProps']['ads'])
-        unparsed_data = rename_columns(unparsed_data)
-        print(unparsed_data.head())
-
-        unparsed_data['region'] = parse_region(url)
-        model_dicts = df_to_sqlmodel_dicts(unparsed_data, Property)
-
-        table_name = get_table_name(url)
-        insert_data_to_db(table_name, model_dicts, Property)
-
-    except json.JSONDecodeError as e:
-        print(f" [!] Failed to decode JSON: {e}")
-    except ValueError as e:
-        print(f" [!] Error: {e}")
+    except (json.JSONDecodeError, KeyError) as e:
+        print(f" [!] Failed to process message: {e}")
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=5672))
     channel = connection.channel()
 
-    channel.queue_declare(queue='scraped-data')
+    channel.queue_declare(queue='scraper-data', durable=True)
 
-    channel.basic_consume(queue='scraped-data', on_message_callback=callback, auto_ack=True)
+    def callback(ch, method, properties, body):
+        handle_message(body)
+
+    channel.basic_consume(queue='scraper-data', on_message_callback=callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
-    channel.start_consuming()
-
-if __name__ == '__main__':
     try:
-        main()
+        channel.start_consuming()
     except KeyboardInterrupt:
         print('Interrupted')
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+        sys.exit(0)
+
+if __name__ == '__main__':
+    main()
--- scraper/scraper_olx.py
+++ scraper/scraper_olx.py
@@ -1,131 +1,167 @@
+import pika
 import json
 import csv
-import re
-import math
+import time
 from multiprocessing import Process
-from selectolax.parser import HTMLParser
-import cloudscraper
-import queue.rabbitmq_client
-import pandas as pd
+from playwright.sync_api import sync_playwright
 
-WORKER_AMOUNT = 4
-INPUT_FILE = 'olx_links.csv'
-RENT_TABLE_NAME = "property_rent"
-SALE_TABLE_NAME = "property_sale"
+NUM_OF_WORKERS = 1
+INPUT_FILE = 'data/olx_links.csv'
 
-RESULTS_PER_PAGE = 50
-
-client = rabbitmq_client.RabbitMQClient()
-
-scraper = cloudscraper.create_scraper(
-        browser={
-            "browser": "chrome",
-            "platform": "windows",
-        },
-    )
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='scraped-data')
+channel.queue_declare(queue='scraping-progress')
 
 def read_csv(filename):
     with open(filename, 'r', newline='') as file:
         reader = csv.DictReader(file, delimiter=',')
         return [row['url'] for row in reader if row['status'] != 'finished']
 
-def get_max_page(url):
-    response = scraper.get(url)
-    html = HTMLParser(response.text)
-    results_string = html.css_first('div#total-of-ads > div > p').text()
-    match = re.search(r"de (\d+) resultados", results_string.replace(".", ""))
+def send_scraped_data(message, url):
+    data = {
+        "url": url,
+        "content": message
+    }
+    channel.basic_publish(exchange='',
+                          routing_key='scraped-data',
+                          body=json.dumps(data))
+    print(f"Data sent to 'scraped-data' for URL: {url}")
 
-    if match:
-        total_results = int(match.group(1))  
-        return math.ceil(total_results / RESULTS_PER_PAGE)
-    else:
-        raise ValueError("Could not find the number of results in the response")
+def send_scraping_status(url, status):
+    data = {
+        "url": url,
+        "status": status
+    }
+    channel.basic_publish(exchange='',
+                          routing_key='scraping-progress',
+                          body=json.dumps(data))
+    print(f"Data sent to 'scraping-progress' for URL: {url}, status: {status}")
 
-def scrape_page(base_url, page_number):
-    url = f'{base_url}{"&" if "?" in base_url else "?"}o={page_number}'
-    response = scraper.get(url) 
-    html = HTMLParser(response.text)
 
-    if response.status_code == 200:
-        results_json = html.css_first('script#__NEXT_DATA__').text()
-        data, ad_type = map_data(url, results_json)
-        client.send_data(url, data, ad_type)
-    else:
-        client.send_status(url, "failed")
+def scrape_url(page, url, browser):
+    scrape_page(page, url)
+    go_to_next_page(page, url, browser)
 
-def map_data(url, json_response):
-    try:
-        data_raw = json.loads(json_response)['props']['pageProps']['ads']
-        data = []
+def go_to_next_page(page, url, browser):
+    max_retries = 5
+    retries = 0
+    while retries < max_retries:
+        try:
+            next_page_button = page.query_selector("text=Próxima página")
+            next_page_url = page.evaluate("(element) => element.closest('a').href", next_page_button) if next_page_button else None
+            if next_page_url:
+                page.close()
+                time.sleep(2)
+                page = open_new_window(next_page_url, browser)
+                scrape_url(page, url, browser)
+                break
+            else:
+                time.sleep(1)
+                page.reload()
+                time.sleep(3)
+        except Exception as e:
+            retries += 1
+            print(f"Error in go_to_next_page: {e} (Retry {retries}/{max_retries})")
+            if "closed" in str(e):
+                break  
+            time.sleep(5)
+            try:
+                page.reload()
+            except:
+                break  
+    if retries == max_retries:
+        print(f"Max retries reached for {url}")
+        send_scraping_status(url, "failed")
 
-        for item in data_raw:
-            item = expand_properties(item)
-            item_mapped = {
-                'listing_id': item.get('listId'),
-                'title': item.get('title'),
-                'url': item.get('url'),
-                'listing_date': item.get('date'),
-                'municipality': item.get('locationDetails', {}).get('municipality'),
-                'neighborhood': item.get('locationDetails', {}).get('neighbourhood'),
-                'state': item.get('locationDetails', {}).get('uf'),
-                'area': item.get('área construída'),
-                'parking': item.get('vagas na garagem'),
-                'bedrooms': item.get('quartos'),
-                'bathrooms': item.get('banheiros'),
-                'condominium_price': item.get('condominio'),
-                'category': item.get('categoria'),
-                'type': item.get('tipo'),
-                'condominium_details': item.get('detalhes do condomínio'),
-                'property_details': item.get('detalhes do imóvel'),
-            }
-            item_mapped['region'] = parse_region(url)
-            data.append(item_mapped)
-
-        ad_type = get_ad_type(url)
-        return data, ad_type
-    except Exception as e:
-        print(f"Error in map_data: {e}")
-        raise
-
-def get_ad_type(url: str) -> str:
-    if "venda" in url:
-        return SALE_TABLE_NAME
-    elif "aluguel" in url:
-        return RENT_TABLE_NAME
-    else:
-        raise ValueError(f"Unknown URL: {url}")
-
-def expand_properties(row):
-    properties = row.get('properties', [])
-    if not isinstance(properties, list):
-        return pd.Series()
-
-    return pd.Series({item['label'].lower(): item['value'] for item in properties})
-
-def parse_region(url):
-    pattern = re.compile(r'/([^?/]+)\?' if '?' in url else r'/([^/]+)$')
-    match = pattern.search(url)
-    return match.group(1).replace('-', ' ') if match else None
+def scrape_page(page, url):
+    max_retries = 3
+    retries = 0
     
-def scrape_url(url):
-    max_page = get_max_page(url)
+    while retries < max_retries:
+        try:
+            page.wait_for_selector('script#__NEXT_DATA__', state='attached', timeout=20000) 
+            script_content = page.evaluate('document.querySelector("script#__NEXT_DATA__").textContent')
+            send_scraped_data(script_content, url)
+            break  
+        except Exception as e:
+            retries += 1
+            print(f"Error in scrape_page: {e} (Retry {retries}/{max_retries})")
+            time.sleep(3)
+            try:
+                page.reload()
+            except:
+                break  
+    else:
+        print(f"Max retries reached for {url}, skipping.")
+        send_scraping_status(url, "failed")
+    
+    if retries == max_retries:
+        print(f"No #__NEXT_DATA__ found for {url}. Closing page.")
+        page.close()
+        send_scraping_status(url, "no_data")
 
-    for page_number in range(1, max_page + 1):
-        scrape_page(url, page_number)
+def open_new_window(url, browser):
+    context = None
+    page = None
+    retries = 0
+    max_retries = 3
+    while retries < max_retries:
+        try:
+            context = browser.new_context(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36")
+            page = context.new_page()
+            page.goto(url, timeout=30000)
+            page.wait_for_load_state('domcontentloaded', timeout=10000)
+            break
+        except Exception as e:
+            print(f"Error opening new window for {url}: {e}")
+            retries += 1
+            if "net::ERR_NAME_NOT_RESOLVED" in str(e):
+                time.sleep(5 * retries) 
+            if page:
+                page.close()
+            if retries == max_retries:
+                raise e  
+    return page
 
 def start_process(urls):
-    for url in urls:
-        try:
-            client.send_status(url, "started")
-            scrape_url(url)
-            client.send_status(url, "finished")
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
 
-        except Exception as e:
-            client.send_status(url, "failed")
+        for url in urls:
+            try:
+                page = open_new_window(url, browser)
+                send_scraping_status(url, "started")
+                print("Starting link:", url)
+                
+                while True:  # Loop through pages instead of recursion
+                    scrape_page(page, url)
+                    next_page_button = page.query_selector("text=Próxima página")
+                    
+                    if not next_page_button:
+                        break  # Exit loop if no next page button
+                    
+                    next_page_url = page.evaluate("(element) => element.closest('a').href", next_page_button)
+                    if not next_page_url:
+                        break
+                    
+                    print(f"Navigating to next page: {next_page_url}")
+                    page.goto(next_page_url)
+                
+                send_scraping_status(url, "finished")
+            except Exception as e:
+                print(f"Error processing {url}: {e}")
+                send_scraping_status(url, "failed")
+            finally:
+                page.close()  # Ensure page is closed after use
+        
+        browser.close()
+    connection.close()
+
 
 if __name__ == "__main__":
     urls = read_csv(INPUT_FILE)
-    url_chunks = [urls[i::WORKER_AMOUNT] for i in range(WORKER_AMOUNT)]
+    url_chunks = [urls[i::NUM_OF_WORKERS] for i in range(NUM_OF_WORKERS)]
 
     processes = []
     for chunk in url_chunks:
